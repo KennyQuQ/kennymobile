d486aaf86647e8de84351de8892fa22bfdafff25
KennyOwO <117258707+KennyQuQ@users.noreply.github.com>
2023-03-09 20:57:47 +0800

    Add files via upload

--- js/placeArrangement.js
+++ js/placeArrangement.js
@@ -1,6 +1,6 @@
 let student = [];
 let number = [];
-const specialDesk = [4,6,7,8,10]
+const specialDesk = [3,4,5]
 var studentText = document.getElementById("inputName")
 
 
@@ -9,12 +9,16 @@ var studentText = document.getElementById("inputName")
 const btnEl = document.querySelector("#btn-el")
 
 // create a student desk array
-for (let i=12; i>=1; i--) {
+for (let i=13; i>=1; i--) {
     student.push(document.querySelector("#seat-"+ i +""))
 }
 
 // button click event
 btnEl.addEventListener("click", generate)
+
+function CSalert() {
+    alert("More Coming Soon! :)");
+}
 
 // generate students array
 function generate() {
@@ -28,8 +32,8 @@ function generate() {
     }
 
     // pick a random number that wont repeat and pick students
-        for (let i=1; i<=parseInt(studentName.length/12)+1; i++) {
-            if(studentName.length >12 && number.length <= 5) {break}
+        for (let i=1; i<=parseInt(studentName.length/13)+1; i++) {
+            if(studentName.length >13 && number.length <= 3) {break}
                 student.forEach(element => {
                         let r = Math.floor(Math.random() * number.length) //random
                         if (studentName[number[r] - 1] !== undefined) {
@@ -41,7 +45,7 @@ function generate() {
                 });
         }
         // pick the remain student to the special desks that contain 3 student
-        for (let i=0; i<=4; i++) {
+        for (let i=0; i<=3; i++) {
             let r = Math.floor(Math.random() * number.length) //random
                 if(studentName[number[r] - 1] !== undefined) {
                     student[specialDesk[i]].textContent += ` ${studentName[number[r] - 1]}` // (${number[r]})
